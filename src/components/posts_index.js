6e0acae3a3abd070323d492836a567990c799603
jonathanlam <lamjwe91@gmail.com>
2017-04-04 21:43:10 -0700

    Fetching data with life cycle method continued

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
+    // => This is a lifecycle method. React will call this automatically whenever the component is about to be rendered
+    // to the DOM for the first time. It will not be called on subsequent rerenders though.
+    // => Because this will only be called once, this will be a good place to put the action creator to go fetch the data.
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>List of blog posts</div>
@@ -8,4 +18,9 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+function mapDispatchToProps(dispatch) {
+    // This gives us access to this.props.fetchPosts
+    return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
