a7c6a0c1268c174a562ce1ae43a5c1943a0b8aa1
jonathanlam <lamjwe91@gmail.com>
2017-04-04 23:40:23 -0700

    Creating forms with redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import PostReducer from './reducer_posts';
 
+// import redux-form, grab the reducer property off of it and then create a variable with it named formReducer. 
+import { reducer as formReducer } from 'redux-form';
+
 const rootReducer = combineReducers({
-  posts: PostReducer
+  posts: PostReducer,
+  form: formReducer
 });
 
 export default rootReducer;
