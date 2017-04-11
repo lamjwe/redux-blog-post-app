a9cc23ce393a6a2ddd4cca6c33ae6be7aef828a2
jonathanlam <lamjwe91@gmail.com>
2017-04-10 23:13:08 -0700

    Loading data on render. Showing post data

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id);
+    }
+
     render() {
         return <div>Show post {this.props.params.id}</div>;
     }
 }
 
-export default PostsShow;
+// Need to do a get request first to show post data.
+// 1. Create action creator in actions/index.js
+// 2. Grab FETCH_POST as action.type in reducer.
+// 3. Make use of the action creator in posts_show, here. 
+export default connect(null, { fetchPost })(PostsShow);
